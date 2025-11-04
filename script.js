fdc0d2551ec22f3bbbefff3e726617cd418f0f8b
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-05 02:52:08 +0530

    add CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+let note = [];
+
+function openNoteDialog() {
+    const dialog = document.getElementById("noteDialog");
+    const contentInput = document.getElementById("noteContent");
+    const titleInput = document.getElementById("noteTitle");
+    console.log(dialog, contentInput, titleInput);
+    
+}
